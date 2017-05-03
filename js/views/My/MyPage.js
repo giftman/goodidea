90925aa269fdd005cd0bc442f07d24bc7e6cd13d
chufan <121551591@qq.com>
2017-05-03 16:42:01 +0800

    bank card

--- js/views/My/MyPage.js
+++ js/views/My/MyPage.js
@@ -226,7 +226,7 @@ class MyPage extends Component {
                   <Image source={require('../../img/shezhi.png')} resizeMode='contain' />
 
                   <View style={styles.item}>
-                    <Text style={styles.itemTitle}>设置(v0.9.0)</Text>
+                    <Text style={styles.itemTitle}>设置(v0.9.1)</Text>
                     <Icon
                       name='ios-arrow-forward'
                       size={25}
